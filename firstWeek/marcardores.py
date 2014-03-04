321c8dd603749e20128756ca17357322379e0318
FraGoTe <fgonzalestello91@gmail.com>
2014-03-03 23:30:55 -0500

    First week classes

---
+++ firstWeek/marcardores.py
@@ -0,0 +1,19 @@
+a = 45
+b = 56
+
+print ('su edad es', a)
+
+print ('la edad es', a, ' de sofia')
+
+print ('Con marcadores su edad es %d'%b)
+
+print ('Su sueldo es %f'%b)
+
+a = 'este es a' 
+
+print (a)
+
+a, b = b, a
+
+print (a)
+print (b)
---
+++ firstWeek/variables.py
@@ -0,0 +1,5 @@
+a = 700
+b = 20
+boolResult = a > b
+print(type(boolResult))
+
